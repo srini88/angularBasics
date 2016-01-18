c626e43345b8dcbda43ac4e76258ed1ea826a005
srini88 <pvsrinivas.ece@gmail.com>
2016-01-18 01:37:05 -0600

    ng-repeat

--- js.js
+++ js.js
@@ -3,6 +3,6 @@ var myApp = angular.module("myApp", []);
 // The angular object is a global object created by the AngularJS JavaScript which is included at the beginning of the page.
 myApp.controller('myController', ['$scope', function($scope){
 	$scope.hello = {};
-	$scope.showIt= true;
+	$scope.arr1= ["apple", "orange", "kiwi"];
 	
 }]);

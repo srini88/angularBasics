de8244b562e9431fb72b9e6adca298b8aac48578
srini88 <pvsrinivas.ece@gmail.com>
2016-01-18 02:02:50 -0600

    myown Filter

--- js.js
+++ js.js
@@ -3,12 +3,15 @@ var myApp = angular.module("myApp", []);
 // The angular object is a global object created by the AngularJS JavaScript which is included at the beginning of the page.
 myApp.controller('myController', ['$scope', function($scope){
 	$scope.hello = {};
+	$scope.sample = "srini is awesome";
 	$scope.arr1= ["apple", "orange", "kiwi"];
-
-	$scope.items  = [ {text : "one"}, {text : "two"}, {text : "three"}, {text : "four"} ];
-	$scope.today = new Date();
-	
 }]);
+
+myApp.filter("myOwnFilter", function(){    // not working when I remove one funct and put everything at root
+	return function(boo){
+		return boo + "  motheryaar";
+	}
+});
 
 // we get op 
 // {"text":"one"}
